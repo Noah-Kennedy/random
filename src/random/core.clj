34884f3f71e1f0a1ab5b8f8acaeab176da908c36
Noah Kennedy <Nomaxx117@gmail.com>
2018-03-28 09:12:04 -0500

    changes

--- src/random/core.clj
+++ src/random/core.clj
@@ -1,34 +1,3 @@
-(ns random.core
-  (:import (java.util ArrayList LinkedList)))
+(ns random.core)
+(pyro.printer/swap-stacktrace-engine!)
 
-(def message-queue (ref (LinkedList.)))
-
-(defn send-to-queue [message]
-  (dosync
-    (.push (deref message-queue) message)))
-
-(defn recieve-from-queue []
-  (dosync
-    (.pop (deref message-queue))))
-
-(defn start-recieving []
-  (.start
-    (Thread.
-      #(dosync
-         (while
-           (not-empty
-             (deref message-queue))
-           (println (recieve-from-queue)))))))
-
-(defn start-sending []
-  (.start
-    (Thread.
-      #(loop [n 0]
-         (send-to-queue n)
-         (when (< n 100)
-           (recur (inc n)))))))
-
-(defn start-both []
-  (do
-    (start-sending)
-    (start-recieving)))
